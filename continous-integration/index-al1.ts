e63ae38b99543ba5f61230049df5e914e4577ab4
Benjamin Genz <benjamin.genz@europace.de>
2021-01-18 10:32:56 +0100

    parallelize AL1/2 tests

--- continous-integration/index-al1.ts
+++ continous-integration/index-al1.ts
@@ -6,11 +6,10 @@ import { App, BundlingDockerImage, Duration, Stack } from '@aws-cdk/core';
 
 const app = new App();
 const stack = new Stack(app, 'tesseract-lambda-ci');
-
+const pathToLayerSource = path.resolve(__dirname, '..');
 /**
  * Test setup and artifacts for AL 1
  */
-const pathToLayerSource = path.resolve(__dirname, '..');
 const al1Layer = new lambda.LayerVersion(stack, 'al1-layer', {
     code: Code.fromAsset(pathToLayerSource, {
     bundling: {
@@ -42,36 +41,4 @@ new lambda.Function(stack, 'python3.6', {
     handler: 'handler.main',
 });
 
-/**
- * Test setup and artifacts for AL 2
- */
-const al2Layer = new lambda.LayerVersion(stack, 'al2-layer', {
-    code: Code.fromAsset(pathToLayerSource, {
-    bundling: {
-        image: BundlingDockerImage.fromAsset(pathToLayerSource, { file: '../Dockerfile.al2' }),
-        command: ['/bin/bash', '-c', 'cp -r /opt/build-dist/. /asset-output/'],
-    },
-    }),
-    description: 'AL2 Tesseract Layer',
-});
-stack.renameLogicalId(stack.getLogicalId(al2Layer.node.defaultChild as CfnLayerVersion), 'al2layer')
-new lambda.Function(stack, 'python3.8', {
-    code: lambda.Code.fromAsset(path.resolve(__dirname, 'lambda-handlers'),
-    {
-        bundling: {
-            image: BundlingDockerImage.fromRegistry('lambci/lambda:build-python3.8'),
-            command: ['/bin/bash', '-c', [
-                'pip install -r requirements.txt -t /asset-output/',
-                'cp faust.png /asset-output',
-                'cp handler.py /asset-output',
-            ].join(' && ')],
-        }
-    }),
-    runtime: Runtime.PYTHON_3_8,
-    layers: [al2Layer],
-    functionName: `al2-py38`,
-    memorySize: 512,
-    timeout: Duration.seconds(30),
-    handler: 'handler.main',
-});
 
