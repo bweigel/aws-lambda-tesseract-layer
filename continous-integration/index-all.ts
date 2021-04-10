40b60fe4c9ba9bc9720cb5fa9605d34a522b2f26
bweigel <benjamin.weigel@europace.de>
2021-04-10 16:24:00 +0200

    use branch for v5

--- continous-integration/index-all.ts
+++ continous-integration/index-all.ts
@@ -73,36 +73,3 @@ new lambda.Function(stack, 'python3.8', {
     timeout: Duration.seconds(30),
     handler: 'handler.main',
 });
-
-/**
- * Test setup and artifacts for AL 2 - Tesseract 5
- */
-const al2tess5Layer = new lambda.LayerVersion(stack, 'al2tess5-layer', {
-    code: Code.fromAsset(pathToLayerSource, {
-    bundling: {
-        image: BundlingDockerImage.fromAsset(pathToLayerSource, { file: '../Dockerfile.al2-tesseract5' }),
-        command: ['/bin/bash', '-c', 'cp -r /opt/build-dist/. /asset-output/'],
-    },
-    }),
-    description: 'AL2 Tesseract 5 Layer',
-});
-stack.renameLogicalId(stack.getLogicalId(al2tess5Layer.node.defaultChild as CfnLayerVersion), 'al2tess5layer')
-new lambda.Function(stack, 'python3.8-tess5', {
-    code: lambda.Code.fromAsset(path.resolve(__dirname, 'lambda-handlers'),
-    {
-        bundling: {
-            image: BundlingDockerImage.fromRegistry('lambci/lambda:build-python3.8'),
-            command: ['/bin/bash', '-c', [
-                'pip install -r requirements.txt -t /asset-output/',
-                'cp faust.png /asset-output',
-                'cp handler.py /asset-output',
-            ].join(' && ')],
-        }
-    }),
-    runtime: Runtime.PYTHON_3_8,
-    layers: [al2tess5Layer],
-    functionName: `al2-tess5-py38`,
-    memorySize: 512,
-    timeout: Duration.seconds(30),
-    handler: 'handler.main',
-});
