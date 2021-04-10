0a66097b271be0f0f4d9add553ec48619b941079
bweigel <benjamin.weigel@europace.de>
2021-04-10 23:00:34 +0200

    update ci workflow

--- create_release_assets.sh
+++ create_release_assets.sh
@@ -3,14 +3,7 @@
 set -euxo
 
 pushd .
-cd ready-to-use/amazonlinux-1
-zip -r ../layer-al1.zip *
-popd
-pushd .
 cd ready-to-use/amazonlinux-2
 zip -r ../layer-al2.zip *
 popd
-pushd .
-cd ready-to-use/amazonlinux-2-tesseract-5
-zip -r ../layer-al2-tesseract-5.zip *
-popd
+
