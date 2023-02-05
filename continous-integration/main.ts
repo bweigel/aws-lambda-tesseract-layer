8456433257be8e3bc98a7cf21c45062a5d26c24e
bweigel <benjamin.weigel@europace.de>
2023-02-05 17:07:34 +0100

    fix: ts error

--- continous-integration/main.ts
+++ continous-integration/main.ts
@@ -11,57 +11,57 @@ const pathToLayerSource = path.resolve(__dirname, '..');
  * Test setup and artifacts for AL 2
  */
 const al2Layer = new lambda.LayerVersion(stack, 'al2-layer', {
-    code: Code.fromAsset(pathToLayerSource, {
-        bundling: {
-            image: DockerImage.fromBuild(pathToLayerSource, { file: 'Dockerfile.al2' }),
-            command: ['/bin/bash', '-c', 'cp -r /opt/build-dist/. /asset-output/'],
-        },
-    }),
-    description: 'AL2 Tesseract Layer',
+  code: Code.fromAsset(pathToLayerSource, {
+    bundling: {
+      image: DockerImage.fromBuild(pathToLayerSource, { file: 'Dockerfile.al2' }),
+      command: ['/bin/bash', '-c', 'cp -r /opt/build-dist/. /asset-output/'],
+    },
+  }),
+  description: 'AL2 Tesseract Layer',
 });
 stack.renameLogicalId(stack.getLogicalId(al2Layer.node.defaultChild as CfnLayerVersion), 'al2layer');
 
 new lambda.Function(stack, 'python3.8', {
-    code: lambda.Code.fromAsset(path.resolve(__dirname, 'lambda-handlers/py38'), {
-        bundling: {
-            image: DockerImage.fromRegistry('public.ecr.aws/sam/build-python3.8:latest'),
-            command: [
-                '/bin/bash',
-                '-c',
-                ['pip install -r requirements.txt -t /asset-output/', 'cp faust.png /asset-output', 'cp handler.py /asset-output'].join(' && '),
-            ],
-        },
-    }),
-    runtime: Runtime.PYTHON_3_8,
-    layers: [al2Layer],
-    functionName: `py38`,
-    memorySize: 512,
-    timeout: Duration.seconds(30),
-    handler: 'handler.main',
+  code: lambda.Code.fromAsset(path.resolve(__dirname, 'lambda-handlers/py38'), {
+    bundling: {
+      image: DockerImage.fromRegistry('public.ecr.aws/sam/build-python3.8:latest'),
+      command: [
+        '/bin/bash',
+        '-c',
+        ['pip install -r requirements.txt -t /asset-output/', 'cp faust.png /asset-output', 'cp handler.py /asset-output'].join(' && '),
+      ],
+    },
+  }),
+  runtime: Runtime.PYTHON_3_8,
+  layers: [al2Layer],
+  functionName: `py38`,
+  memorySize: 512,
+  timeout: Duration.seconds(30),
+  handler: 'handler.main',
 });
 
 new nodelambda.NodejsFunction(stack, 'node16', {
-    bundling: {
-        nodeModules: ['tesseractocr'],
-        commandHooks: {
-            beforeInstall() {
-                return [];
-            },
-            beforeBundling(inputDir: string, outputDir: string): string[] {
-                return [`cp ${inputDir}/faust.png ${outputDir}`];
-            },
-            afterBundling(inputDir: string, outputDir: string): string[] {
-                return [];
-            },
-        },
+  bundling: {
+    nodeModules: ['tesseractocr'],
+    commandHooks: {
+      beforeInstall() {
+        return [];
+      },
+      beforeBundling(inputDir: string, outputDir: string): string[] {
+        return [`cp ${inputDir}/faust.png ${outputDir}`];
+      },
+      afterBundling(): string[] {
+        return [];
+      },
     },
-    depsLockFilePath: path.resolve(__dirname, 'lambda-handlers/node16/package-lock.json'),
+  },
+  depsLockFilePath: path.resolve(__dirname, 'lambda-handlers/node16/package-lock.json'),
 
-    runtime: Runtime.NODEJS_16_X,
-    entry: path.resolve(__dirname, 'lambda-handlers/node16/index.js'),
-    layers: [al2Layer],
-    functionName: `node16`,
-    memorySize: 512,
-    timeout: Duration.seconds(30),
-    handler: 'handler',
+  runtime: Runtime.NODEJS_16_X,
+  entry: path.resolve(__dirname, 'lambda-handlers/node16/index.js'),
+  layers: [al2Layer],
+  functionName: `node16`,
+  memorySize: 512,
+  timeout: Duration.seconds(30),
+  handler: 'handler',
 });
